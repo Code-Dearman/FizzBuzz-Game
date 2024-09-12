01ade725d020c11fb37e53c3ae1649d5a53bff51
Isaac Dearman <isaac.dearman@gmail.com>
2024-09-12 13:35:21 +0000

    Added notes to JS file. Changed margin in CSS file.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,15 @@
-function updateOutput() {
+function updateOutput() {                                               /*defines the function updateOutput which is called but the GO! button in FizzBuzzgame.html*/
+    
+    var i;                                                              /*Creates the variable i which denotes the integer in the FizzBuzz game*/
+    var userInput = document.getElementById("userValue").value;         /*Creates the variable of userInput which takes the value from the userInput element in FizzBuzzgame.html*/
+                                                                        
+    for (i = 1; i <= userInput; i++) {                                  /*Changes i value to 1 and states, where i is less than or equal to the userInput value, add 1 to i*/
+        let output = "";                                                /*defines a new variable, output, which is a blank string for now*/
+        if (i % 3 === 0) output += "Fizz";                              /*if the remainder of i / 3 is exactly equal to 0 add the string "Fizz" to the variable output*/
+        if (i % 5 === 0) output += "Buzz";                              /*if the remainder of i / 5 is exactly equal to 0 add the string "Buzz" to the variable output*/
+        if (output === "") output += i;                                 /*if neither of the previous rules apply add the current value of i to the output*/
 
-    var i;
-    var userInput = document.getElementById("userValue").value;
-
-    for (i = 1; i <= userInput; i++) {
-        let output = "";
-        if (i % 3 === 0) output += "Fizz";
-        if (i % 5 === 0) output += "Buzz";
-        if (output === "") output += i;
-
-        document.getElementById("output").innerHTML += output + " ";
-        console.log(output);
+        document.getElementById("output").innerHTML += output + " ";    /*Targets the html element with the id of "output" and adds the output variable + a blank space string to the inner html*/
     }
 
 }
-
-
