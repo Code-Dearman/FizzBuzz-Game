870738ffbd2e68313a7893d2809fab79881eecf3
Isaac Dearman <isaac.dearman@gmail.com>
2024-10-15 20:08:27 +0000

    add timing to updateOutput function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,10 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function updateOutput() {                                                                       /*defines the function updateOutput which is called by the GO! button in FizzBuzzgame.html*/
     
-    var i;                                                                                      /*Creates the variable i which denotes the integer in the FizzBuzz game*/
-    var userInput = document.getElementById("userValue").value;                                 /*Creates the variable of userInput which takes the value from the userInput element in FizzBuzzgame.html*/
+    let i;                                                                                      /*Creates the variable i which denotes the integer in the FizzBuzz game*/
+    let userInput = document.getElementById("userValue").value;                                 /*Creates the variable of userInput which takes the value from the userInput element in FizzBuzzgame.html*/
 
-    if (userInput > 10000) {                                                                    /*Sets an upper limit of 10,000 so that the user cannot input large numbers and cause performance issues.*/
+    let start = performance.now();                                                              /*Takes a time stamp at the start of the function*/                                                     
+
+    if (userInput > 20000) {                                                                    /*Sets an upper limit of 10,000 so that the user cannot input large numbers and cause performance issues.*/
         document.getElementById("output").textContent += "Error: Please don't break me";        /*Displays an error message if the user does try to input a large number.*/
         return;                                                                                 /*Ends the script early*/
     }                                                
@@ -32,12 +34,17 @@ function updateOutput() {                                                       
         document.getElementById("goButton").style.display="none"
     }
 
+    let timeTaken = performance.now() - start;                                                  /*Takes a times stamp at the end of the function and minuses the original time to find how long the function took to run.*/
+    const timeReport = `That function took ${timeTaken} milliseconds to run!`;                  /*Writes a template literal which reports the time taken to run the function and applies it to a constant*/
+    document.getElementById("timeReport").innerText = timeReport;                               /*applies the timeReport to the timeStamp paragraph*/
+
 }
 
 function clearOutput() {                                                                        /*Defines function clearOutput which is called by the reset button in FizzBuzzgame.html*/
     document.getElementById("output").textContent = "";                                         /*Targets the textContent of the P element which has the id"output" and changes it to "" or blank*/
-    document.getElementById("valueDiv").style.display="block";                                   /*Brings back the user input box*/
-    document.getElementById("goButton").style.display="inline-block";
+    document.getElementById("valueDiv").style.display="block";                                  /*Brings back the user input box*/
+    document.getElementById("goButton").style.display="inline-block";                           /*Brings back the goButton*/
+    document.getElementById("timeReport").innerText = "";
     
 }
 
